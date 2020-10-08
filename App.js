db8c66c275695cedc0b09e3c7d9827f414781b2f
Marisa Li <marisa.susan.li@gmail.com>
2020-10-08 17:55:20 -0400

    firebase config

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
+// @refresh reset
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import { firebaseConfig } from './firebaseConfig';
+import Camera from './components/Camera';
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+const db = firebase.firestore();
+
+db.collection('users')
+  .add({
+    email: 'newtest@test.com',
+    password: '12345678',
+    username: 'newUser2',
+  })
+  .then(function (docRef) {
+    console.log('Document written with ID: ', docRef.id);
+  })
+  .catch(function (error) {
+    console.error('Error adding document: ', error);
+  });
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <View style={styles.container}>
+    //   <Text>You have arrived at the Postcard app</Text>
+
+    //   <StatusBar style='auto' />
+    // </View>
+    <View>
+      <Camera />
     </View>
   );
 }
