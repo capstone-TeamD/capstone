3db06c7979de0fb1f201f47ccc8d181ddab12799
Melissa <66856935+mlam0527@users.noreply.github.com>
2020-10-09 17:01:04 -0400

    profile base layout

--- components/Profile.js
+++ components/Profile.js
@@ -1,18 +1,49 @@
 import { StatusBar } from "expo-status-bar";
 import React, { Component } from "react";
+import cameraicon from '../assets/cameraicon.png';
 import {
   View,
   StyleSheet,
   Text,
-} from "react-native";
+  ScrollView,
+  Image
+} from "react-native"
 
 
 export default class Profile extends Component {
-
   render() {
     return (
       <View style={styles.container}>
-          <Text>Profile</Text>
+        <View style={styles.header}>
+          <Image
+            style={styles.icon}
+            source={cameraicon}
+            />
+          <View style={styles.info}>
+            <Text>username</Text>
+            <Text>description</Text>
+          </View>
+        </View>
+        <ScrollView vertical style={styles.gallery}>
+          <Text>Gallery Scrolling</Text>
+          {/* postcards */}
+          <Image
+                style={styles.icon}
+                source={cameraicon}
+                />
+          <Image
+                style={styles.icon}
+                source={cameraicon}
+                />
+          <Image
+                style={styles.icon}
+                source={cameraicon}
+                />
+          <Image
+                style={styles.icon}
+                source={cameraicon}
+                />
+        </ScrollView>
       </View>
     );
   }
@@ -20,9 +51,30 @@ export default class Profile extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'gray',
+      alignItems: 'stretch',
+      justifyContent: 'center',
+      // width: 50
+    },
+    gallery: {
+      flex: 5,
+      backgroundColor: 'lightblue'
+    },
+    header: {
+      flex: 0.4,
+      flexDirection: 'row',
+      alignContent: 'flex-end',
+      backgroundColor: 'lightpink'
+    },
+    icon: {
+      width: 80,
+      height: 80,
+      marginTop: 50,
+      marginHorizontal: 25
+    },
+    info: {
+      marginTop: 50
     }
 })
