3d0162b114998a0cf81b24bb9daf5b6c16cf09d5
Marisa Li <marisa.susan.li@gmail.com>
2020-10-09 18:01:13 -0400

    added navigation tabs

--- App.js
+++ App.js
@@ -7,6 +7,19 @@ import 'firebase/firestore';
 import { firebaseConfig } from './firebaseConfig';
 import SwitchNavigator from './components/SwitchNavigator';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Profile from './components/Profile';
+import Mailbox from './components/Mailbox';
+import Upload from './components/CameraIP';
+import Discover from './components/Discover';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
 }
@@ -26,10 +39,65 @@ const db = firebase.firestore();
 //   });
 
 export default function App() {
+  const isLoggedin = true;
+
   return (
-    <View style={styles.container}>
-      <SwitchNavigator />
-    </View>
+    <NavigationContainer>
+      {isLoggedin ? (
+        <Tab.Navigator>
+          <Tab.Screen
+            name='Profile'
+            component={Profile}
+            options={{
+              tabBarLabel: 'Profile',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons
+                  name='face-profile'
+                  color={color}
+                  size={26}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name='Mailbo'
+            component={Mailbox}
+            options={{
+              tabBarLabel: 'Mailbox',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons
+                  name='mailbox'
+                  color={color}
+                  size={26}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name='Mailbox'
+            component={Upload}
+            options={{
+              tabBarLabel: 'Upload',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name='camera' color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name='Discover'
+            component={Discover}
+            options={{
+              tabBarLabel: 'Discover',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name='earth' color={color} size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      ) : (
+        <SwitchNavigator />
+      )}
+    </NavigationContainer>
   );
 }
 
