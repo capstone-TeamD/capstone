651708f22fadd17b31ba9cda86514cbdf220201c
Ellen Kwan <eh2.kwan@gmail.com>
2020-10-12 10:06:57 -0700

    galleryView

--- components/GalleryGrid.js
+++ components/GalleryGrid.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Dimensions, FlatList, Image, StyleSheet } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React from "react";
+import { Dimensions, FlatList, Image, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-import { formatPhotoUri } from '../picsum';
+import { formatPhotoUri } from "../picsum";
 
 export default function PhotoGrid({ photos, numColumns, onEndReached }) {
-  const { width } = Dimensions.get('window');
+  const { width } = Dimensions.get("window");
 
   const size = width / numColumns;
 
@@ -32,14 +32,12 @@ export default function PhotoGrid({ photos, numColumns, onEndReached }) {
 }
 
 const styles = StyleSheet.create({
-  photo: {
-    // backgroundColor: 'black',
-
-    width: '100%',
-    height: '100%',
-  },
   container: {
-    marginBottom: 15,
-    height: '100%',
+    justifyContent: "center",
+    marginBottom: 7,
+    width: "100%",
+  },
+  photo: {
+    height: 130,
   },
 });
