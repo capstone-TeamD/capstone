01555a88dc1b2c120febb72e85a05f78add77b5c
Ellen Kwan <eh2.kwan@gmail.com>
2020-10-16 12:14:49 -0700

    feat: discover UI add

--- components/GalleryGrid.js
+++ components/GalleryGrid.js
@@ -25,6 +25,7 @@ export default function PhotoGrid({ photos, numColumns, onEndReached }) {
       onEndReached={onEndReached}
       renderItem={({ item }) => (
         <TouchableOpacity key={item.id} style={styles.container}>
+          <View style={styles.photoContainer}>
           <Image
             key={item.id}
             style={styles.photo}
@@ -35,7 +36,8 @@ export default function PhotoGrid({ photos, numColumns, onEndReached }) {
           />
 
           {/* <Image style={styles.icon} source={cameraicon} /> */}
-          <Text>Postcard created by: {`${item.id}`}</Text>
+          <Text style={styles.userText}>Created By: {`${item.username}`}</Text>
+          </View>
         </TouchableOpacity>
       )}
     />
@@ -45,12 +47,32 @@ export default function PhotoGrid({ photos, numColumns, onEndReached }) {
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
+    alignItems: "center",
     marginBottom: 7,
     width: '100%',
+    marginTop: 10,
+  },
+  photoContainer: {
+    width: "90%",
+    backgroundColor: "white",
+    borderColor: "black",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000000",
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    shadowOffset: {
+      height: 1,
+      width: 1
+    }
   },
   photo: {
+    marginTop: 10,
     backgroundColor: 'black',
-    height: 130,
+    height: 150,
+    width: "90%",
+    alignSelf: "center",
   },
   icon: {
     width: 80,
@@ -58,4 +80,11 @@ const styles = StyleSheet.create({
     marginTop: 25,
     marginHorizontal: 30,
   },
+  userText: {
+    marginHorizontal: 5,
+    justifyContent: "center",
+    alignContent: "center",
+    padding: 3,
+    fontSize: 11,
+  }
 });
